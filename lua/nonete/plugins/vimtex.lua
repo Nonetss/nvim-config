231f533a1b4227cfffdea1f296dee88c9b048466
Nonetss <amorenolopezbarajas@gmail.com>
2024-10-20 17:05:55 +0200

    Cambiamos el renderizado de vimtex

--- lua/nonete/plugins/vimtex.lua
+++ lua/nonete/plugins/vimtex.lua
@@ -3,6 +3,6 @@ return {
 	lazy = false, -- lazy-loading will disable inverse search
 	config = function()
 		vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
-		vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+		vim.g.vimtex_quickfix_method = vim.fn.executable("latexmk -lualatex") == 1 and "pplatex" or "latexlog"
 	end,
 }
